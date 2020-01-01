404edfcc04a814c89006ac1863df15cff0145254
Hyunwoo Cho <barefoot1423@gmail.com>
2020-01-01 16:31:22 -0500

    done 115p

--- src/main/java/com/hyunwoo/book/springboot/web/PostApiController.java
+++ src/main/java/com/hyunwoo/book/springboot/web/PostApiController.java
@@ -1,11 +1,11 @@
 package com.hyunwoo.book.springboot.web;
 
 import com.hyunwoo.book.springboot.service.posts.PostsService;
+import com.hyunwoo.book.springboot.web.dto.PostsResponseDto;
 import com.hyunwoo.book.springboot.web.dto.PostsSaveRequestDto;
+import com.hyunwoo.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -13,8 +13,18 @@ public class PostApiController {
 
     private final PostsService postsService;
 
-    @PutMapping("/api/v1/posts")
+    @PostMapping("/api/v1/posts")
     public Long save(@RequestBody PostsSaveRequestDto postsSaveRequestDto) {
         return postsService.save(postsSaveRequestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id) {
+        return postsService.findById(id);
     }
 }
