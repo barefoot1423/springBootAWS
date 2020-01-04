69d18226c1b6dc5c3727c1bcdecd6c7c22c801bd
Hyunwoo Cho <barefoot1423@gmail.com>
2020-01-04 15:54:33 -0500

    done until 150P

--- src/main/java/com/hyunwoo/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/hyunwoo/book/springboot/service/posts/PostsService.java
@@ -3,11 +3,15 @@ package com.hyunwoo.book.springboot.service.posts;
 import com.hyunwoo.book.springboot.domain.posts.Posts;
 import com.hyunwoo.book.springboot.domain.posts.PostsRepository;
 import com.hyunwoo.book.springboot.web.dto.PostsResponseDto;
+import com.hyunwoo.book.springboot.web.dto.PostsListResponseDto;
 import com.hyunwoo.book.springboot.web.dto.PostsSaveRequestDto;
 import com.hyunwoo.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -34,4 +38,12 @@ public class PostsService {
 
         return new PostsResponseDto(entity);
     }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+            .map(PostsListResponseDto::new)
+            .collect(Collectors.toList());
+    }
+
 }
