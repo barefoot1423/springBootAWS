a55cffdc4123d299641f2d2432429de5ba3bddd9
Hyunwoo Cho <barefoot1423@gmail.com>
2020-01-04 18:17:42 -0500

    done 200p

--- src/main/java/com/hyunwoo/book/springboot/web/IndexController.java
+++ src/main/java/com/hyunwoo/book/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.hyunwoo.book.springboot.web;
 
+import com.hyunwoo.book.springboot.config.auth.LoginUser;
 import com.hyunwoo.book.springboot.config.auth.dto.SessionUser;
 import com.hyunwoo.book.springboot.domain.user.User;
 import com.hyunwoo.book.springboot.service.posts.PostsService;
@@ -20,18 +21,13 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
-
         if (user != null) {
             model.addAttribute("userName", user.getName());
         }
-
         return "index";
     }
-
     @GetMapping("/posts/save")
     public String postsSave() {
         return "posts-save";
